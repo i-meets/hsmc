7a61a39aadb12fa3f732eb4b9067c4916151e256
meets <andrements@gmail.com>
2021-09-05 20:07:20 -0300

    Projeto criado

---
+++ src/main/java/com/hard/hsmc/HsmcApplication.java
@@ -0,0 +1,13 @@
+package com.hard.hsmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hard/hsmc/HsmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hard.hsmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
